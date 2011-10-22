9f489962e6a9e610ae20e735e85f9bf4c404d133
Bernd Herzog <bernd@nachregenkommtsonne.de>
2011-10-22 21:53:25 +0200

    git-svn-id: svn+ssh://blue.3dallee.de/bernd/C%23/HLGraph@62 533b6b32-d271-4e35-8edc-18ab391a8b2f

---
+++ HLGraph/frmMain.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace HLGraph
+{
+	partial class frmMain
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.ucGraph1 = new HLGraph.ucGraph();
+			this.SuspendLayout();
+			// 
+			// ucGraph1
+			// 
+			this.ucGraph1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.ucGraph1.Location = new System.Drawing.Point(0, 0);
+			this.ucGraph1.Name = "ucGraph1";
+			this.ucGraph1.Size = new System.Drawing.Size(841, 715);
+			this.ucGraph1.TabIndex = 0;
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(841, 715);
+			this.Controls.Add(this.ucGraph1);
+			this.Name = "frmMain";
+			this.Text = "HL Graph";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			this.Load += new System.EventHandler(this.frmMain_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private ucGraph ucGraph1;
+	}
+}
+
---
+++ HLGraph/ucGraph.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HLGraph
+{
+	public partial class ucGraph : UserControl
+	{
+
+
+		public ucGraph()
+		{
+			InitializeComponent();
+		}
+
+
+
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			try
+			{
+
+
+				base.OnPaint(e);
+				e.Graphics.Clear(Color.White);
+				Pen pen = new Pen(Color.Black);
+
+				for (int i = 0; i < this.Width; i++)
+				{
+					double x = ItoD(i, this.Width);
+					//int iX = (int)Math.Floor(x);
+					if (x == 0)
+						continue;
+
+
+					double y = (Math.Pow(-1, x) * x * x * x + x) / (x * x * x + 1);
+					if (!double.IsNaN(y))
+					{
+						int j = DToI(y, this.Height);
+						e.Graphics.DrawLine(pen, KarPToScreen(new Point(i, j)), KarPToScreen(new Point(i, 0)));
+					}
+				}
+
+				//e.Graphics.DrawLine(pen, KarPToScreen(new Point(50, 50)), KarPToScreen(new Point(100, 100)));
+			}
+			catch
+			{
+
+
+			}
+		}
+
+		double ItoD(int i, int pb)
+		{
+			if (i < pb / 2)
+			{
+				return (double)i / (pb / 2);
+			}
+			else
+			{
+				double f = (double)(pb - i) / (pb / 2);
+				if (f == 0)
+					return pb;
+				return 1 / f;
+			}
+		}
+
+		int DToI(double d, int pb)
+		{
+			if (d < 1)
+			{
+				return (int)Math.Floor(d * pb / 2);
+			}
+			else
+			{
+				return (pb - (int)Math.Floor((1 / d) * (pb / 2)));
+			}
+		}
+
+		Point KarPToScreen(Point p)
+		{
+			return new Point(p.X, this.Height - p.Y);
+		}
+	}
+}
